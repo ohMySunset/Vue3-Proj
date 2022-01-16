c74a9227b8ca69bce0eb11885510fff3b514608e
ohMySunset <znfnznfnakqj@naver.com>
2022-01-16 23:33:54 +0900

    json-server로 비동기 통신하기

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,6 @@
       v-if : 조건에 맞는 엘리먼트만 판별하여 렌더링해줌
     -->
     <div class="container">
-      <h4>count: {{ count }}</h4>
-      <h4>doubleCountComputed: {{ doubleCountComputed }}</h4>
-      <h4>doubleCountMethod: {{ doubleCountMethod() }}</h4>
-      <button @click="count++">Plus One</button>
       <h2>To-Do List</h2>  
        <input 
           class="form-control"
@@ -19,6 +15,7 @@
         >
         <hr/>
       <TodoSimpleForm @add-todo="addTodo"/>
+      <div style="color: red">{{ error }}</div>
       <div v-if="!filterdTodos.length">
         There is nothing to display.
       </div>  
@@ -26,7 +23,7 @@
         :todos="filterdTodos" 
         @toggle-Todo="toggleTodo"
         @delete-todo="deleteTodo"
-        /> <!-- 부모컴포넌트 -> 자식 컴포넌트로 데이터 이동 -->
+        /> <!-- props : 부모컴포넌트 -> 자식 컴포넌트로 데이터 이동 -->
     </div>
   </div>
 </template>
@@ -35,6 +32,7 @@
 import { ref, computed } from 'vue'; 
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -44,25 +42,57 @@ export default {
   setup() {
     const toggle = ref(false);
     const todos = ref([]);
+    const error = ref('');
     const todoStyle = {
       textDecoration: 'line-through',
       color: 'gray'
     };
 
+    const getTodos = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/todos');
+        todos.value = res.data;
+      } catch (err) {
+        console.log(err);
+        error.value = 'Something went wrong.';
+      } 
+    };
+
+    getTodos();
+
     const toggleTodo = (index) => { // 자식 컴포넌트에서 받은 데이터가 들어옴
       todos.value[index].completed = !todos.value[index].completed;
-    }
+    };
 
-    const addTodo = (todo) => { // 자식 컴포넌트에서 받은 데이터가 들어옴
-      todos.value.push(todo);
+    const addTodo = async (todo) => { // 자식 컴포넌트에서 받은 데이터가 들어옴
+      // 데이터베이스에 todo를 저장
+      error.value = '';
+      try {
+        const res = await axios.post('http://localhost:3000/todos', {
+        subject: todo.subject,
+        completed: todo.completed,
+      });
+        todos.value.push(res.data);
+      } catch (err) {
+         console.log(err);
+         error.value = 'Something went wrong.';
+      }
     }; 
 
     const onToggle = () => {
       toggle.value = !toggle.value
-    }
+    } 
 
-    const deleteTodo = (index) => { // 자식 컴포넌트에서 받은 데이터가 들어옴
-      todos.value.splice(index, 1);
+    const deleteTodo = async (index) => { // 자식 컴포넌트에서 받은 데이터가 들어옴
+      error.value = '';
+      const id = todos.value[index].id;
+      try {
+        await axios.delete('http://localhost:3000/todos/' + id);
+        todos.value.splice(index, 1);
+      } catch (err) {
+        console.log(err);
+        error.value = 'Something went wrong.';
+      }
     }
 
     const count = ref(1);
@@ -97,6 +127,7 @@ export default {
       doubleCountMethod,
       searchText,
       filterdTodos,
+      error,
     }
 
   }
